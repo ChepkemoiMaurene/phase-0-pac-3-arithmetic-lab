ea0a69a49327980f74d7193f9646d8129811a28f
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-08-18 17:39:06 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(){
+}
+function subtract(){
+}
+function multiply(){
+}
+function divide(){
+}
+function add(a,b){
+    let result= a+b;
+    return result;
+}
+add(1,2)
+function subtract(a, b){
+    return a-b
+}
+subtract(2,1)
+function multiply(a,b){
+    return a*b
+    
+}
+multiply(3,2)
+
+
+function divide(a,b){
+
+    return a/b
+}
+function increment(n){
+    return ++n
+}
+
+
+function decrement (n){
+
+    return --n
+}
+function makeInt(n){
+   return parseInt(n,10)
+    
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
